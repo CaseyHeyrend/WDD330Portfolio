8415f0d46d9093aa39df9cc1eed64b10234a4a11
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-02-03 20:42:59 -0700

    Week 5

---
+++ Week05/TeamActivity/main.js
@@ -0,0 +1,7 @@
+import HikesController from './HikeController.js';
+
+//on load grab the array and insert it into the page
+window.addEventListener("load", () => {
+  const hikeController = new HikesController('#hikes');
+  hikeController.showHikeList();
+});
